6a3bd87f258f20e350409b1738e4896044581ed4
StepanMelnik <stepan.melnik@viskan.se>
2017-08-10 17:17:18 +0200

    Add static params to send to logstash

--- src/main/java/com/viskan/logstash/appender/LogstashAppender.java
+++ src/main/java/com/viskan/logstash/appender/LogstashAppender.java
@@ -11,6 +11,10 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LocationInfo;
@@ -18,299 +22,325 @@ import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
 
 /**
- * Log4j appender that appends logs to Logstash in a JSON-format that is very
- * easy for Logstash to consume and dump directly into Elastic Search, for example.
+ * <p>
+ * Log4j appender that appends logs to Logstash in a JSON-format<br/>
+ * that is very easy for Logstash to consume and dump directly into Elastic Search, for example.
+ * </p>
  *
  * @author Anton Johansson
  */
 public class LogstashAppender extends AppenderSkeleton
 {
-	private static final String TRUNCATED_BY_LOGSTASH_APPENDER = "...[truncated by logstash appender]";
-	private static final int TRUNCATE_MSG_LENGTH = TRUNCATED_BY_LOGSTASH_APPENDER.length();
-	
-	private String application;
-	private String environment;
-	private String logstashHost = "";
-	private int logstashPort;
-	private String mdcKeys = "";
-	private boolean appendClassInformation;
-	private int stacktraceLength = Integer.MIN_VALUE;
+    private static final String TRUNCATED_BY_LOGSTASH_APPENDER = "...[truncated by logstash appender]";
+    private static final int TRUNCATE_MSG_LENGTH = TRUNCATED_BY_LOGSTASH_APPENDER.length();
 
-	private DatagramSocket socket;
-	private InetAddress address;
-	private String[] actualMdcKeys;
+    private String application;
+    private String environment;
+    private String logstashHost = "";
+    private int logstashPort;
+    private String mdcKeys = "";
+    private String parameters = "";
+    private boolean appendClassInformation;
+    private int stacktraceLength = Integer.MIN_VALUE;
 
-	public void setApplication(String application)
-	{
-		this.application = application;
-	}
+    private DatagramSocket socket;
+    private InetAddress address;
+    private String[] actualMdcKeys;
+    private Map<String, String> actualParameters = new HashMap<>();
 
-	public void setEnvironment(String environment)
-	{
-		this.environment = environment;
-	}
+    public void setApplication(String application)
+    {
+        this.application = application;
+    }
 
-	public void setLogstashHost(String logstashHost)
-	{
-		this.logstashHost = logstashHost;
-	}
+    public void setEnvironment(String environment)
+    {
+        this.environment = environment;
+    }
 
-	public void setLogstashPort(int logstashPort)
-	{
-		this.logstashPort = logstashPort;
-	}
+    public void setLogstashHost(String logstashHost)
+    {
+        this.logstashHost = logstashHost;
+    }
 
-	public void setMdcKeys(String mdcKeys)
-	{
-		this.mdcKeys = mdcKeys;
-	}
+    public void setLogstashPort(int logstashPort)
+    {
+        this.logstashPort = logstashPort;
+    }
 
-	public void setAppendClassInformation(boolean appendClassInformation)
-	{
-		this.appendClassInformation = appendClassInformation;
-	}
-	
-	public void setStacktraceLength(int stacktraceLength)
-	{
-		this.stacktraceLength = stacktraceLength;
-	}
+    public void setMdcKeys(String mdcKeys)
+    {
+        this.mdcKeys = mdcKeys;
+    }
 
-	/**
-	 * Appends given logging event to Logstash.
-	 */
-	@Override
-	protected void append(LoggingEvent event)
-	{
-		if (socket == null)
-		{
-			return;
-		}
+    public void setAppendClassInformation(boolean appendClassInformation)
+    {
+        this.appendClassInformation = appendClassInformation;
+    }
 
-		String data = getData(event);
-		byte[] buffer = data.getBytes(forName("UTF-8"));
+    public void setStacktraceLength(int stacktraceLength)
+    {
+        this.stacktraceLength = stacktraceLength;
+    }
 
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, logstashPort);
-		try
-		{
-			socket.send(packet);
-		}
-		catch (IOException e)
-		{
-			System.err.println("Could not send UDP packet");
-		}
-	}
+    public void setParameters(String parameters)
+    {
+        this.parameters = parameters;
+    }
 
-	private String getData(LoggingEvent event)
-	{
-		StringBuilder data = new StringBuilder();
-		addValue(data, "message", event.getMessage());
-		addValue(data, "name", event.getLoggerName());
-		addValue(data, "severity", event.getLevel().getSyslogEquivalent());
-		addValue(data, "severityText", event.getLevel());
-		
-		if (application != null)
-		{
-			addValue(data, "application", application);
-		}
-		
-		if (environment != null)
-		{
-			addValue(data, "environment", environment);
-		}
+    /**
+     * Appends given logging event to Logstash.
+     */
+    @Override
+    protected void append(LoggingEvent event)
+    {
+        if (socket == null)
+        {
+            return;
+        }
 
-		if (appendClassInformation)
-		{
-			LocationInfo locationInformation = event.getLocationInformation();
-			addValue(data, "className", locationInformation.getClassName());
-			addValue(data, "lineNumber", locationInformation.getLineNumber());
-		}
+        String data = getData(event);
+        byte[] buffer = data.getBytes(forName("UTF-8"));
 
-		ThrowableInformation throwableInformation = event.getThrowableInformation();
-		if (throwableInformation != null)
-		{
-			addValue(data, "stacktrace", getStacktrace(throwableInformation.getThrowable()));
-		}
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, logstashPort);
+        try
+        {
+            socket.send(packet);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not send UDP packet");
+        }
+    }
 
-		for (String mdcKey : actualMdcKeys)
-		{
-			Object mdcValue = event.getMDC(mdcKey);
-			if (mdcValue != null)
-			{
-				addValue(data, mdcKey, mdcValue);
-			}
-		}
+    private String getData(LoggingEvent event)
+    {
+        StringBuilder data = new StringBuilder();
+        addValue(data, "message", event.getMessage());
+        addValue(data, "name", event.getLoggerName());
+        addValue(data, "severity", event.getLevel().getSyslogEquivalent());
+        addValue(data, "severityText", event.getLevel());
 
-		return data.append("}").toString();
-	}
+        if (application != null)
+        {
+            addValue(data, "application", application);
+        }
 
-	private void addValue(StringBuilder data, String key, Object value)
-	{
-		boolean isFirstValue = data.toString().isEmpty();
-		boolean isNumber = value instanceof Number;
+        if (environment != null)
+        {
+            addValue(data, "environment", environment);
+        }
 
-		if (isFirstValue)
-		{
-			data.append("{");
-		}
-		else
-		{
-			data.append(",");
-		}
+        if (appendClassInformation)
+        {
+            LocationInfo locationInformation = event.getLocationInformation();
+            addValue(data, "className", locationInformation.getClassName());
+            addValue(data, "lineNumber", locationInformation.getLineNumber());
+        }
 
-		data.append("\"")
-			.append(escape(key))
-			.append("\":");
-		
-		if (!isNumber)
-		{
-			data.append("\"");
-		}
-		
-		if (value != null)
-		{
-			data.append(escape(value.toString()));
-		}
-		
-		if (!isNumber)
-		{
-			data.append("\"");
-		}
-	}
+        ThrowableInformation throwableInformation = event.getThrowableInformation();
+        if (throwableInformation != null)
+        {
+            addValue(data, "stacktrace", getStacktrace(throwableInformation.getThrowable()));
+        }
 
-	private Object getStacktrace(Throwable throwable)
-	{
-		try (StringWriter stringWriter = new StringWriter(); PrintWriter printWriter = new PrintWriter(stringWriter))
-		{
-			throwable.printStackTrace(printWriter);
-			String stackTrace = stringWriter.toString();
-			if (stacktraceLength >= 0)
-			{
-				return stackTrace.substring(0, min(stackTrace.length(), stacktraceLength) - TRUNCATE_MSG_LENGTH) + TRUNCATED_BY_LOGSTASH_APPENDER;
-			}
-			return stackTrace;
-		}
-		catch (IOException e)
-		{
-			System.err.println("Error when fetching stacktrace");
-			return null;
-		}
-	}
+        for (String mdcKey : actualMdcKeys)
+        {
+            Object mdcValue = event.getMDC(mdcKey);
+            if (mdcValue != null)
+            {
+                addValue(data, mdcKey, mdcValue);
+            }
+        }
 
-	/**
-	 * We use UDP when appending to Logstash, so we do not need to close the connection.
-	 *
-	 */
-	@Override
-	public void close()
-	{
-		if (socket != null)
-		{
-			socket.close();
-		}
-	}
+        actualParameters.forEach((k, v) ->
+        {
+            addValue(data, k, v);
+        });
 
-	/**
-	 * This appender does not require a layout, hence we return {@code false}.
-	 *
-	 * @return Returns {@code false}.
-	 */
-	@Override
-	public boolean requiresLayout()
-	{
-		return false;
-	}
+        return data.append("}").toString();
+    }
 
-	/**
-	 * Creates the UDP socket.
-	 */
-	@Override
-	public void activateOptions()
-	{
-		actualMdcKeys = mdcKeys.replaceAll(" ", "").split(",");
+    private void addValue(StringBuilder data, String key, Object value)
+    {
+        boolean isFirstValue = data.toString().isEmpty();
+        boolean isNumber = value instanceof Number;
 
-		try
-		{
-			address = InetAddress.getByName(logstashHost);
-			socket = new DatagramSocket();
-		}
-		catch (UnknownHostException e)
-		{
-			System.err.println("Could not find host: " + logstashHost);
-		}
-		catch (SocketException e)
-		{
-			System.err.println("Could not create UDP socket");
-		}
-	}
+        if (isFirstValue)
+        {
+            data.append("{");
+        }
+        else
+        {
+            data.append(",");
+        }
 
-	/**
-	 * Escape quotes, \, /, \r, \n, \b, \f, \t and other control characters (U+0000 through U+001F).
-	 * <p>
-	 * Taken from the JSONValue class of json-simple-1.1.jar
-	 *
-	 * @param s
-	 * @return
-	 */
-	//CSOFF
-	private String escape(String s){
-		if(s==null)
-		{
-			return null;
-		}
+        data.append("\"")
+                .append(escape(key))
+                .append("\":");
+
+        if (!isNumber)
+        {
+            data.append("\"");
+        }
+
+        if (value != null)
+        {
+            data.append(escape(value.toString()));
+        }
+
+        if (!isNumber)
+        {
+            data.append("\"");
+        }
+    }
+
+    private Object getStacktrace(Throwable throwable)
+    {
+        try (StringWriter stringWriter = new StringWriter(); PrintWriter printWriter = new PrintWriter(stringWriter))
+        {
+            throwable.printStackTrace(printWriter);
+            String stackTrace = stringWriter.toString();
+            if (stacktraceLength >= 0)
+            {
+                return stackTrace.substring(0, min(stackTrace.length(), stacktraceLength) - TRUNCATE_MSG_LENGTH) + TRUNCATED_BY_LOGSTASH_APPENDER;
+            }
+            return stackTrace;
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error when fetching stacktrace");
+            return null;
+        }
+    }
+
+    /**
+     * We use UDP when appending to Logstash, so we do not need to close the connection.
+     */
+    @Override
+    public void close()
+    {
+        if (socket != null)
+        {
+            socket.close();
+        }
+    }
+
+    /**
+     * This appender does not require a layout, hence we return {@code false}.
+     *
+     * @return Returns {@code false}.
+     */
+    @Override
+    public boolean requiresLayout()
+    {
+        return false;
+    }
+
+    /**
+     * Creates the UDP socket.
+     */
+    @Override
+    public void activateOptions()
+    {
+        actualMdcKeys = mdcKeys.replaceAll(" ", "").split(",");
+
+        actualParameters = Arrays.asList(parameters.split("&"))
+                .stream()
+                .map(s -> s.split("=", 2))
+                .filter(p -> p != null && p.length == 2)
+                .collect(Collectors.toMap(a -> a[0], a -> a[1]));
+
+        try
+        {
+            address = InetAddress.getByName(logstashHost);
+            socket = new DatagramSocket();
+        }
+        catch (UnknownHostException e)
+        {
+            System.err.println("Could not find host: " + logstashHost);
+        }
+        catch (SocketException e)
+        {
+            System.err.println("Could not create UDP socket");
+        }
+    }
+
+    /**
+     * Escape quotes, \, /, \r, \n, \b, \f, \t and other control characters (U+0000 through U+001F).
+     * <p>
+     * Taken from the JSONValue class of json-simple-1.1.jar
+     *
+     * @param s
+     * @return
+     */
+    //CSOFF
+    private String escape(String s)
+    {
+        if (s == null)
+        {
+            return null;
+        }
         StringBuffer sb = new StringBuffer();
         escape(s, sb);
         return sb.toString();
     }
 
-	/**
-	 * Taken from JSONValue class of json-simple-1.1.jar
-	 *
+    /**
+     * Taken from JSONValue class of json-simple-1.1.jar
+     *
      * @param s - Must not be null.
      * @param sb
      */
-    private void escape(String s, StringBuffer sb) {
-		for(int i=0;i<s.length();i++){
-			char ch=s.charAt(i);
-			switch(ch){
-			case '"':
-				sb.append("\\\"");
-				break;
-			case '\\':
-				sb.append("\\\\");
-				break;
-			case '\b':
-				sb.append("\\b");
-				break;
-			case '\f':
-				sb.append("\\f");
-				break;
-			case '\n':
-				sb.append("\\n");
-				break;
-			case '\r':
-				sb.append("\\r");
-				break;
-			case '\t':
-				sb.append("\\t");
-				break;
-			case '/':
-				sb.append("\\/");
-				break;
-			default:
-                //Reference: http://www.unicode.org/versions/Unicode5.1.0/
-				if((ch>='\u0000' && ch<='\u001F') || (ch>='\u007F' && ch<='\u009F') || (ch>='\u2000' && ch<='\u20FF')){
-					String ss=Integer.toHexString(ch);
-					sb.append("\\u");
-					for(int k=0;k<4-ss.length();k++){
-						sb.append('0');
-					}
-					sb.append(ss.toUpperCase());
-				}
-				else{
-					sb.append(ch);
-				}
-			}
-		}//for
-	}
+    private void escape(String s, StringBuffer sb)
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            switch (ch)
+            {
+                case '"':
+                    sb.append("\\\"");
+                    break;
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                case '/':
+                    sb.append("\\/");
+                    break;
+                default:
+                    //Reference: http://www.unicode.org/versions/Unicode5.1.0/
+                    if ((ch >= '\u0000' && ch <= '\u001F') || (ch >= '\u007F' && ch <= '\u009F') || (ch >= '\u2000' && ch <= '\u20FF'))
+                    {
+                        String ss = Integer.toHexString(ch);
+                        sb.append("\\u");
+                        for (int k = 0; k < 4 - ss.length(); k++)
+                        {
+                            sb.append('0');
+                        }
+                        sb.append(ss.toUpperCase());
+                    }
+                    else
+                    {
+                        sb.append(ch);
+                    }
+            }
+        }//for
+    }
     //CSON
 }
--- src/test/java/com/viskan/logstash/appender/LogstashAppenderTest.java
+++ src/test/java/com/viskan/logstash/appender/LogstashAppenderTest.java
@@ -16,119 +16,174 @@ import org.junit.Test;
  */
 public class LogstashAppenderTest extends Assert
 {
-	private static final Logger LOGGER = Logger.getLogger(LogstashAppenderTest.class);
+    private static final Logger LOGGER = Logger.getLogger(LogstashAppenderTest.class);
 
-	@Test
-	public void test_appender_with_details() throws IOException
-	{
-		configure(true);
-		
-		byte[] received = new byte[1024];
-		DatagramPacket packet = new DatagramPacket(received, received.length);
-		
-		try (DatagramSocket socket = new DatagramSocket(12345))
-		{
-			new Thread(new LoggerTask(true)).start();
-			
-			socket.setSoTimeout(50000);
-			socket.receive(packet);
-			
-			String actual = new String(received, 0, packet.getLength());
-			String expected = getExpectedWithDetails();
-			
-			assertEquals(expected, actual);
-		}
-	}
+    @Test
+    public void test_appender_with_details() throws IOException
+    {
+        configure(true, false);
 
-	@Test
-	public void test_appender_without_details() throws IOException
-	{
-		configure(false);
-		
-		byte[] received = new byte[1024];
-		DatagramPacket packet = new DatagramPacket(received, received.length);
-		
-		try (DatagramSocket socket = new DatagramSocket(12345))
-		{
-			new Thread(new LoggerTask(false)).start();
-			
-			socket.setSoTimeout(50000);
-			socket.receive(packet);
-			
-			String actual = new String(received, 0, packet.getLength());
-			String expected = getExpectedWithoutDetails();
-			
-			assertEquals(expected, actual);
-		}
-	}
+        byte[] received = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(received, received.length);
 
-	private void configure(boolean details)
-	{
-		LogstashAppender appender = new LogstashAppender();
-		appender.setLogstashHost("localhost");
-		appender.setLogstashPort(12345);
-		
-		if (details)
-		{
-			appender.setApplication("Test");
-			appender.setEnvironment("Development");
-			appender.setAppendClassInformation(true);
-			appender.setMdcKeys("key1,key2,key3");
-		}
-		
-		appender.activateOptions();
-		
-		Logger.getRootLogger().removeAllAppenders();
-		Logger.getRootLogger().addAppender(appender);
-	}
+        try (DatagramSocket socket = new DatagramSocket(12345))
+        {
+            new Thread(new LoggerTask(true)).start();
 
-	private String getExpectedWithDetails()
-	{
-		return "{\"message\":\"Some message\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":3,\"severityText\":\"ERROR\",\"application\":\"Test\",\"environment\":\"Development\",\"className\":\"com.viskan.logstash.appender.LogstashAppenderTest$LoggerTask\",\"lineNumber\":\"124\",\"stacktrace\":\"java.lang.Exception: Error\\r\\n\\tat com.viskan.logstash.appender.LogstashAppenderTest$LoggerTask.run(LogstashAppenderTest.java:124)\\r\\n\\tat java.lang.Thread.run(Thread.java:744)\\r\\n\",\"key2\":\"value2\"}";
-	}
+            socket.setSoTimeout(50000);
+            socket.receive(packet);
 
-	private String getExpectedWithoutDetails()
-	{
-		return "{\"message\":\"Hello World!\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":6,\"severityText\":\"INFO\"}";
-	}
-	
-	/**
-	 * Task for executing the logging.
-	 *
-	 * @author Anton Johansson
-	 */
-	private static class LoggerTask implements Runnable
-	{
-		boolean details;
-		
-		private LoggerTask(boolean details)
-		{
-			this.details = details;
-		}
-		
-		@Override
-		public void run()
-		{
-			try
-			{
-				Thread.sleep(200);
-			}
-			catch (InterruptedException e)
-			{
-			}
+            String actual = new String(received, 0, packet.getLength());
+            String expected = getExpectedWithDetails();
 
-			if (details)
-			{
-				MDC.put("key2", "value2");
-				MDC.put("key4", "value4");
-				LOGGER.error("Some message", new Exception("Error"));
-				MDC.remove("key2");
-				MDC.remove("key4");
-			}
-			else
-			{
-				LOGGER.info("Hello World!");
-			}
-		}
-	}
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void test_appender_without_details() throws IOException
+    {
+        configure(false, false);
+
+        byte[] received = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(received, received.length);
+
+        try (DatagramSocket socket = new DatagramSocket(12345))
+        {
+            new Thread(new LoggerTask(false)).start();
+
+            socket.setSoTimeout(50000);
+            socket.receive(packet);
+
+            String actual = new String(received, 0, packet.getLength());
+            String expected = getExpectedWithoutDetails();
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void test_empty_static_parameters() throws Exception
+    {
+        configure(false, false);
+
+        byte[] received = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(received, received.length);
+
+        try (DatagramSocket socket = new DatagramSocket(12345))
+        {
+            new Thread(() ->
+            {
+                LOGGER.info("Test empty static keys!");
+            }).start();
+
+            socket.setSoTimeout(50000);
+            socket.receive(packet);
+
+            String actual = new String(received, 0, packet.getLength());
+            String expected = "{\"message\":\"Test empty static keys!\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":6,\"severityText\":\"INFO\"}";
+
+            assertEquals("Expect the correct logger message", expected, actual);
+        }
+    }
+
+    @Test
+    public void test_static_parameters() throws Exception
+    {
+        configure(false, true);
+
+        byte[] received = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(received, received.length);
+
+        try (DatagramSocket socket = new DatagramSocket(12345))
+        {
+            new Thread(() ->
+            {
+                LOGGER.info("Test static keys!");
+            }).start();
+
+            socket.setSoTimeout(50000);
+            socket.receive(packet);
+
+            String actual = new String(received, 0, packet.getLength());
+            String expected = "{\"message\":\"Test static keys!\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":6,\"severityText\":\"INFO\",\"group2\":\"REQUEST\",\"group\":\"PSP\"}";
+
+            assertEquals("Expect the correct logger message", expected, actual);
+        }
+    }
+
+    private void configure(boolean details, boolean parameters)
+    {
+        LogstashAppender appender = new LogstashAppender();
+        appender.setLogstashHost("localhost");
+        appender.setLogstashPort(12345);
+
+        if (details)
+        {
+            appender.setApplication("Test");
+            appender.setEnvironment("Development");
+            appender.setAppendClassInformation(true);
+            appender.setMdcKeys("key1,key2,key3");
+        }
+
+        if (parameters)
+        {
+            appender.setParameters("group=PSP&group2=REQUEST");
+        }
+
+        appender.activateOptions();
+
+        Logger.getRootLogger().removeAllAppenders();
+        Logger.getRootLogger().addAppender(appender);
+    }
+
+    private String getExpectedWithDetails()
+    {
+        return "{\"message\":\"Some message\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":3,\"severityText\":\"ERROR\",\"application\":\"Test\",\"environment\":\"Development\",\"className\":\"com.viskan.logstash.appender.LogstashAppenderTest$LoggerTask\",\"lineNumber\":\"179\",\"stacktrace\":\"java.lang.Exception: Error\\r\\n\\tat com.viskan.logstash.appender.LogstashAppenderTest$LoggerTask.run(LogstashAppenderTest.java:179)\\r\\n\\tat java.lang.Thread.run(Thread.java:745)\\r\\n\",\"key2\":\"value2\"}";
+    }
+
+    private String getExpectedWithoutDetails()
+    {
+        return "{\"message\":\"Hello World!\",\"name\":\"com.viskan.logstash.appender.LogstashAppenderTest\",\"severity\":6,\"severityText\":\"INFO\"}";
+    }
+
+    /**
+     * Task for executing the logging.
+     *
+     * @author Anton Johansson
+     */
+    private static class LoggerTask implements Runnable
+    {
+        boolean details;
+
+        private LoggerTask(boolean details)
+        {
+            this.details = details;
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                Thread.sleep(200);
+            }
+            catch (InterruptedException e)
+            {
+            }
+
+            if (details)
+            {
+                MDC.put("key2", "value2");
+                MDC.put("key4", "value4");
+                LOGGER.error("Some message", new Exception("Error"));
+                MDC.remove("key2");
+                MDC.remove("key4");
+            }
+            else
+            {
+                LOGGER.info("Hello World!");
+            }
+        }
+    }
 }
